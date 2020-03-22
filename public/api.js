b8a52153bcef5dfb3e88cfd9dbdc2d65b005eaa4
Nancy Alter <nancy.alter@gmail.com>
2020-03-21 23:22:31 -0400

    Update api.js
    update api.js remove comments add error checked code

--- public/api.js
+++ public/api.js
@@ -1,19 +1,15 @@
 const API = {
+
   async getLastWorkout() {
-    const res = await fetch("/api/workouts");
+    let res;
+    try {
+      res = await fetch("/api/workouts");
+    } catch (err) {
+      console.log(err)
+    }
     const json = await res.json();
 
     return json[json.length - 1];
-  // async getLastWorkout() {
-  //   let res;
-  //   try {
-  //     res = await fetch("/api/workouts");
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  //   const json = await res.json();
-
-  //   return json[json.length - 1];
   },
   async addExercise(data) {
     const id = location.search.split("=")[1];
